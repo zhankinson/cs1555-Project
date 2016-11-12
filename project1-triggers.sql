580af8926924c9ff7f23dde80a5fb940c068e53f
Timothy Kang <timothykang8@gmail.com>
2016-11-12 13:38:07 -0500

    fixed price from pricing

--- project1-triggers.sql
+++ project1-triggers.sql
@@ -1,6 +1,6 @@
 create or replace view completeTickets 
 as select *
-from Pricing a, Flight b, Reservation_detail c, Reservation d
+from Price a, Flight b, Reservation_detail c, Reservation d
 where (a.departure_city = b.departure_city AND a.arrival_city = b.arrival_city) 
 	AND (b.flight_number = c.flight_number) 
 	AND (c.reservation_number = d.reservation_number);
