7f1cc7803db550751462f65dcdb746271dc9068b
Timothy Kang <timothykang8@gmail.com>
2016-11-12 13:35:20 -0500

    creating a view to test

--- project1-db.sql
+++ project1-db.sql
@@ -103,11 +103,3 @@ create or replace table Date(
     primary key (c_date)
 );
 
-//price -> flight (arrival and departure) -> reversation detail -> reservation
-create or replace trigger adjustTicket
-after update 
-on Price
-for each row
-begin
-	update Reservation set Cost = (Select )
-end;
--- project1-triggers.sql
+++ project1-triggers.sql
@@ -1,3 +1,11 @@
+create or replace view completeTickets 
+as select *
+from Pricing a, Flight b, Reservation_detail c, Reservation d
+where (a.departure_city = b.departure_city AND a.arrival_city = b.arrival_city) 
+	AND (b.flight_number = c.flight_number) 
+	AND (c.reservation_number = d.reservation_number);
+
+
 //price -> flight (arrival and departure) -> reversation detail -> reservation
 create or replace trigger adjustTicket
 after update 
