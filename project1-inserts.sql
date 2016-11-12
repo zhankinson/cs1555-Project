90406dd5f297ac84c0ae8594003df304d2f0ee72
Timothy Kang <timothykang8@gmail.com>
2016-11-12 14:00:28 -0500

    added 5 customers to inserts

--- project1-inserts.sql
+++ project1-inserts.sql
@@ -1,1 +1,5 @@
-insert into
+insert into Customer values (100, 'Mr.', 'Gerard', 'Butler', 1111111111111111, to_date('JAN-2020', 'MON-YYYY'), '123 Sweet Dr', 'Pittsburgh', 'PA', '1234567890', 'gbutler@gmail.com', '00000');
+insert into Customer values (101, 'Mrs.', 'Sophia', 'Butler', 1111111111111112, to_date('JAN-2020', 'MON-YYYY'), '123 Sweet Dr', 'Pittsburgh', 'PA', '1234567891', 'sbutler@gmail.com', '00000');
+insert into Customer values (100, 'Mr.', 'Corny', 'Names', 1111111111111113, to_date('NOV-2020', 'MON-YYYY'), '678 Ohara Rd', 'Pittsburgh', 'PA', '1234567892', 'cornNames@gmail.com', '00001');
+insert into Customer values (100, 'Mrs.', 'Funny', 'Names', 1111111111111114, to_date('NOV-2020', 'MON-YYYY'), '678 Ohara Rd', 'Pittsburgh', 'PA', '1234567893', 'funNames@gmail.com', '00001');
+insert into Customer values (100, 'Mr.', 'Noh', 'Waife', 1111111111111115, to_date('MAR-2020', 'MON-YYYY'), '987 Mckee Pl', 'Pittsburgh', 'PA', '1234567894', 'noWife@gmail.com', '00002');
