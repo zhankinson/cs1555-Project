273c8ae9e4eb1aa4d61d7adb9afb95371dd24d36
zhankinson <zah15@pitt.edu>
2016-11-12 14:06:10 -0500

    Changed to single quotes

--- project1-inserts.sql
+++ project1-inserts.sql
@@ -4,13 +4,13 @@ insert into Customer values (100, 'Mr.', 'Corny', 'Names', 1111111111111113, to_
 insert into Customer values (100, 'Mrs.', 'Funny', 'Names', 1111111111111114, to_date('NOV-2020', 'MON-YYYY'), '678 Ohara Rd', 'Pittsburgh', 'PA', '1234567893', 'funNames@gmail.com', '00001');
 insert into Customer values (100, 'Mr.', 'Noh', 'Waife', 1111111111111115, to_date('MAR-2020', 'MON-YYYY'), '987 Mckee Pl', 'Pittsburgh', 'PA', '1234567894', 'noWife@gmail.com', '00002');
 
-insert into Airline values ("00001", "United Airlines", "UAL", 1931);
-insert into Airline values ("00002", "All Nippon Airways", "ANA", 1952);
-insert into Airline values ("00003", "Delta Air Lines", "DAL", 1924);
-insert into Airline values ("00004", "Jet Blue", "JBA", 1998);
-insert into Airline values ("00005", "American Airlines", "AAL", 1930);
-insert into Airline values ("00006", "Frontier Airlines", "FAL", 1994);
-insert into Airline values ("00007", "Southwest Airlines", "SWA", 1967);
-insert into Airline values ("00008", "Virgin Airlines", "VAL", 1984);
-insert into Airline values ("00009", "Spirit Airlines", "SAL", 1980);
-insert into Airline values ("00010", "Alaska Airlines", "AKA", 1932);
+insert into Airline values ('00001', 'United Airlines', 'UAL', 1931);
+insert into Airline values ('00002', 'All Nippon Airways', 'ANA', 1952);
+insert into Airline values ('00003', 'Delta Air Lines', 'DAL', 1924);
+insert into Airline values ('00004', 'Jet Blue', 'JBA', 1998);
+insert into Airline values ('00005', 'American Airlines', 'AAL', 1930);
+insert into Airline values ('00006', 'Frontier Airlines', 'FAL', 1994);
+insert into Airline values ('00007', 'Southwest Airlines', 'SWA', 1967);
+insert into Airline values ('00008', 'Virgin Airlines', 'VAL', 1984);
+insert into Airline values ('00009', 'Spirit Airlines', 'SAL', 1980);
+insert into Airline values ('00010', 'Alaska Airlines', 'AKA', 1932);
